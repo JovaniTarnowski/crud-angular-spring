5b09914321b85262a34e6abc8432f11667750b59
jovani <jovanitranowski@gmail.com>
2022-05-30 19:36:35 -0300

    Optimized imports

--- crud-spring/src/main/java/com/tarnowski/crudspring/model/Student.java
+++ crud-spring/src/main/java/com/tarnowski/crudspring/model/Student.java
@@ -2,7 +2,13 @@ package com.tarnowski.crudspring.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 
 @Data
